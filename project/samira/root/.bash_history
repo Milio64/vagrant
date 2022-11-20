54bfe981834df05c3954ee849e5c79906b38d6cf
milio64 <emile@vanzeijl.net>
2022-11-20 23:43:09 +0100

    win linux vm works

--- project/samira/root/.bash_history
+++ project/samira/root/.bash_history
@@ -1,32 +1,11 @@
-ll
-cd /
-ll
-cd vagrant
-ll
-sh vagrantsetup2.sh 
-cd /root/
-ll
-rm vagrantsetup2.started 
-sh /vagrantsetup2.sh 
-sh ../vagrantsetup2.sh 
-sh /vagrant/vagrantsetup2.sh 
-ll
-cd /vagrant
-ll
- vagrant halt
-cd /vagrant
-ll
-ls
 shutdown -H
-ping 192.168.178.26
 cd /vagrant
-ll
-sh vagrantsetup-samira.sh 
+cat /vargrant/vagrantsetup-samira.sh
+less /root/setup.log
+
+ping 192.168.178.26
 systemctl restart httpd
 systemctl status httpd
-firewall-cmd --add-port=80/tcp --permanent
-firewall-cmd --add-port=443/tcp --permanent
-firewall-cmd --reload 
-#na reload zijn poorten pas te zien in running config
-firewall-cmd --list-all-zone
-exit
+ls
+ll
+history
--- project/samira/vagrantsetup-samira.sh
+++ project/samira/vagrantsetup-samira.sh
@@ -8,8 +8,6 @@ sudo [ -f /root/vagrantsetup-$1.done ] && exit 0
 [ -f /root/secret.sh ] && . /root/secret.sh
 . /etc/os-release
 
-#kopieren van je HTTP voorbeeld file naar de VM webserver
-cp -r /vagrant/var/. /var
 
 echo Start initial installation steps
 case "$ID_LIKE" in
@@ -46,12 +44,20 @@ sudo $installtool install -y $package
 echo start and enable $package
 sudo systemctl enable $package && sudo systemctl start $package
 
-#firewall op VM aanpassen zodat verbinding op http en https poorten gemaakt mogen worden.
+echo firewall op VM aanpassen zodat verbinding op http en https poorten gemaakt mogen worden.
 firewall-cmd --add-port=80/tcp --permanent
 firewall-cmd --add-port=443/tcp --permanent
 firewall-cmd --reload 
-#na reload zijn poorten pas te zien in running config
-firewall-cmd --list-all-zones
+echo na reload zijn poorten pas te zien in running config
+
+#firewall-cmd --list-all-zones
 #testen van firewall poorten indien nodig
 #ss -tulp | grep LIST
 
+#kopieren van je HTTP voorbeeld file naar de VM webserver
+#Nu pas kopieren want dan bestaat directory en defaut rechten zijn geregeld...
+echo 'cp -r /vagrant/var/www/html/index.html /var/www/html/index.html' > /root/copy-html.sh
+chmod 744 /var/www/html/index.html /root/copy-html.sh
+
+echo 'vagrantsetup-$1.done' > /root/vagrantsetup-$1.done ]
+sudo [ -f /root/vagrantsetup-$1.done ] && rm -f /root/vagrantsetup-$1.started
