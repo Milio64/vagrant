3912175d0c44897d387bd5bbbcb73e748e89c63c
U-EMILEWERKKAMER\Emile <emile@vanzeijl.net>
2022-12-02 17:25:39 +0100

    new stuff

--- project/samira/vagrantsetup-samira.sh
+++ project/samira/vagrantsetup-samira.sh
@@ -8,29 +8,17 @@ sudo [ -f /root/vagrantsetup-$1.done ] && exit 0
 [ -f /root/secret.sh ] && . /root/secret.sh
 . /etc/os-release
 
-
 echo Start initial installation steps
 case "$ID_LIKE" in
     "rhel centos fedora")
       installtool=yum
       package=httpd
-
-      #sudo yum update -y
-      #sudo yum install -y httpd
-      #echo start and enable httpd 
-      #sudo systemctl enable httpd && sudo systemctl start httpd
     ;;
       
     "suse opensuse")
       installtool=zypper
       package=apache2
-
       sudo $installtool refresh
-    
-      #sudo zypper update -y
-      #sudo zypper install -y apache2
-      #echo start and enable httpd
-      #sudo systemctl enable httpd && sudo systemctl start httpd
     ;;
   
     *)
@@ -45,10 +33,10 @@ echo start and enable $package
 sudo systemctl enable $package && sudo systemctl start $package
 
 echo firewall op VM aanpassen zodat verbinding op http en https poorten gemaakt mogen worden.
-firewall-cmd --add-port=80/tcp --permanent
-firewall-cmd --add-port=443/tcp --permanent
-firewall-cmd --reload 
-echo na reload zijn poorten pas te zien in running config
+sudo firewall-cmd --add-port=80/tcp --permanent
+sudo firewall-cmd --add-port=443/tcp --permanent
+sudo firewall-cmd --reload 
+echo na reload van de firewall zijn de poorten pas te zien in running config
 
 #firewall-cmd --list-all-zones
 #testen van firewall poorten indien nodig
@@ -56,8 +44,8 @@ echo na reload zijn poorten pas te zien in running config
 
 #kopieren van je HTTP voorbeeld file naar de VM webserver
 #Nu pas kopieren want dan bestaat directory en defaut rechten zijn geregeld...
-echo 'cp -r /vagrant/var/www/html/index.html /var/www/html/index.html' > /root/copy-html.sh
-chmod 744 /var/www/html/index.html /root/copy-html.sh
+sudo echo 'cp -r /vagrant/var/www/html/index.html /var/www/html/index.html' > /root/copy-html.sh
+sudo chmod 744 /var/www/html/index.html /root/copy-html.sh
 
-echo 'vagrantsetup-$1.done' > /root/vagrantsetup-$1.done ]
+sudo echo 'vagrantsetup-$1.done' > /root/vagrantsetup-$1.done ]
 sudo [ -f /root/vagrantsetup-$1.done ] && rm -f /root/vagrantsetup-$1.started
